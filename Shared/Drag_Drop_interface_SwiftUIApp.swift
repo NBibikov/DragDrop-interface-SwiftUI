3daf8840f50828e803fa32cded439503d1bd646f
Mykola Bibikov <bibikov.mykola@softwarium.net>
2022-04-02 15:18:34 +0300

    Initial Commit

---
+++ Shared/Drag_Drop_interface_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_Drop_interface_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Mykola Bibikov on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Drag_Drop_interface_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
